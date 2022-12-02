4b4a2543dcb73ca1993faea81b1d9e340b07627b
Istvan Kleijn <istvankleijn@users.noreply.github.com>
2022-12-02 15:41:29 +0000

    1-2: Calorie Counting

--- src/01-calorie_counting.py
+++ src/01-calorie_counting.py
@@ -15,8 +15,7 @@ test_data = """
 10000
 """
 test_answer = 24000
-
-repr(test_data)
+test_answer_2 = 45000
 
 def most_calories(inventories):
     most_so_far = 0
@@ -35,8 +34,32 @@ def most_calories(inventories):
 
 assert most_calories(test_data) == test_answer
 
+
+test_elf = """
+1000
+2000
+3000
+"""
+
+def calories_per_elf(elf):
+    return sum(int(x) for x in elf.strip().split("\n"))
+
+assert calories_per_elf(test_elf) == 6000
+
+def top_three_sum(text):
+    elves = text.split("\n\n")
+    calories = [calories_per_elf(elf) for elf in elves]
+    calories.sort(reverse=True)
+    return sum(calories[:3])
+
+assert top_three_sum(test_data) == test_answer_2
+
 with open("./data/01-calorie_counting.txt", "r", encoding="utf-8") as file:
     input = file.read()
 
 answer = most_calories(input)
 print(answer)
+
+answer_2 = top_three_sum(input)
+print(answer_2)
+
