6e1128e8c6f9931e7ffc74409a3525d3e8648077
Istvan Kleijn <istvankleijn@users.noreply.github.com>
2022-12-13 13:59:51 +0000

    8-1: Treetop Tree House

--- src/08-treetop_tree_house.py
+++ src/08-treetop_tree_house.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 SHORTEST = 0
 TALLEST = 9
 
@@ -8,55 +11,6 @@ example_heights = [
     [3, 3, 5, 4, 9],
     [3, 5, 3, 9, 0],
 ]
-
-
-def from_left(a):
-    m = len(a[0])
-    n = len(a)
-    visible = [[False for i in range(m)] for row in a]
-    for i, row in enumerate(a):
-        max_height = SHORTEST - 1
-        j = 0
-        while j < m and max_height < TALLEST:
-            if (height := row[j]) > max_height:
-                visible[i][j] = True
-                max_height = height
-            j += 1
-    return visible
-
-
-def from_right(a):
-    m = len(a[0])
-    n = len(a)
-    visible = [[False for i in range(m)] for row in a]
-    for i, row in enumerate(a):
-        max_height = SHORTEST - 1
-        j = 0
-        while j < m and max_height < TALLEST:
-            if (height := row[m - j - 1]) > max_height:
-                visible[i][m - j - 1] = True
-                max_height = height
-            j += 1
-    return visible
-
-
-def from_top(a):
-    m = len(a[0])
-    n = len(a)
-    visible = [[False for i in range(m)] for row in a]
-    max_heights = [SHORTEST - 1 for i in range(m)]
-    for i, row in enumerate(a):
-        for j, height in enumerate(row):
-            if height > max_heights[j]:
-                visible[i][j] = True
-                max_heights[j] = height
-        if max_heights == [TALLEST for i in range(m)]:
-            break
-    return visible
-
-
-def from_bottom(a):
-    return list(reversed(from_top(list(reversed(a)))))
 
 
 def visible(a):
@@ -99,41 +53,6 @@ def visible(a):
     return visible
 
 
-print(from_left(example_heights))
-print(from_right(example_heights))
-print(from_top(example_heights))
-print(from_bottom(example_heights))
-
-print(visible(example_heights))
-
-assert from_left(example_heights) == [
-    [True, False, False, True, False],
-    [True, True, False, False, False],
-    [True, False, False, False, False],
-    [True, False, True, False, True],
-    [True, True, False, True, False],
-]
-assert from_right(example_heights) == [
-    [False, False, False, True, True],
-    [False, False, True, False, True],
-    [True, True, False, True, True],
-    [False, False, False, False, True],
-    [False, False, False, True, True],
-]
-assert from_top(example_heights) == [
-    [True, True, True, True, True],
-    [False, True, True, False, False],
-    [True, False, False, False, False],
-    [False, False, False, False, True],
-    [False, False, False, True, False],
-]
-assert from_bottom(example_heights) == [
-    [False, False, False, False, False],
-    [False, False, False, False, False],
-    [True, False, False, False, False],
-    [False, False, True, False, True],
-    [True, True, True, True, True],
-]
 assert visible(example_heights) == [
     [True, True, True, True, True],
     [True, True, True, False, True],
@@ -141,3 +60,22 @@ assert visible(example_heights) == [
     [True, False, True, False, True],
     [True, True, True, True, True],
 ]
+
+
+def get_answer1(a):
+    return sum(sum(row) for row in visible(a))
+
+
+assert get_answer1(example_heights) == 21
+
+heights = []
+with open("./data/08-treetop_tree_house.txt", "r") as handle:
+    for line in handle:
+        row = [int(x) for x in line.strip()]
+        heights.append(row)
+
+answer1 = get_answer1(heights)
+
+answer2 = None
+
+print(answer1, answer2)
